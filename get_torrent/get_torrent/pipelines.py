fa331fb506a34b452ee88c8f5633f5b27b7d53b7
yienge <yienge@gmail.com>
2014-10-08 15:27:28 +0800

    remove result files from git and add save_data_to_elastic_search method

--- get_torrent/get_torrent/pipelines.py
+++ get_torrent/get_torrent/pipelines.py
@@ -6,6 +6,8 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import sys
+import requests
+import json
 
 if sys.getdefaultencoding() != 'utf-8':
     reload(sys)
@@ -31,7 +33,8 @@ class FilterWordsPipeline(object):
 
             if link_title and "magnet" not in link_title and "谷歌" not in link_title:
                 self.save_all_data(url, link_title)
-                self.save_specific_data(url, link_title)
+                self.save_data_with_kw(url, link_title)
+                self.save_data_to_elastic_search(url, link_title)
 
         return item
 
@@ -41,7 +44,7 @@ class FilterWordsPipeline(object):
             'URL: ' + self.url_prefix + url.strip() + '\n\n'
         )
 
-    def save_specific_data(self, url, link_title):
+    def save_data_with_kw(self, url, link_title):
         # translation_group = ['Dymy']
         title_keywords = ['hunter', 'fairy', 'haikyuu']
 
@@ -54,14 +57,17 @@ class FilterWordsPipeline(object):
                     'URL: ' + self.url_prefix + url.strip() + '\n\n'
                 )
 
+    def save_data_to_elastic_search(self, url, link_title):
+        payload = json.dumps({
+            'link': link_title,
+            'url': self.url_prefix + url.strip()
+        })
+
+        requests.post('http://127.0.0.1:9200/dmhy/torrent', data=payload)
+        pass
+
 
 class FileWriterPipeline(object):
-
-    def process_item(self, item, spider):
-        return item
-
-
-class DBWriterPipeline(object):
 
     def process_item(self, item, spider):
         return item
