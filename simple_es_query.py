6d4cb77a6eaad414debfeead78ba6138b396e0fa
yienge <yienge@gmail.com>
2014-10-08 16:31:20 +0800

    add default query behavior and query count

--- simple_es_query.py
+++ simple_es_query.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import requests
-import pprint
+# import pprint
+
+
+def process_result(r):
+    raw_res = r.json()
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(raw_res['hits']['hits'])
+
+    for item in raw_res['hits']['hits']:
+        result = item['_source']
+        print result['link']
+        print result['url']
+        print
 
 keyword = raw_input('query:')
-r = requests.get('http://127.0.0.1:9200/dmhy/torrent/_search?q=' + keyword)
-raw_res = r.json()
+count = raw_input('result number:')
+if keyword:
+    r = requests.get('http://127.0.0.1:9200/dmhy/torrent/_search?q=' + keyword)
+    process_result(r)
+else:
+    if not count:
+        count = '50'
 
-pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(raw_res['hits']['hits'])
-
-for item in raw_res['hits']['hits']:
-    result = item['_source']
-    print result['url']
-    print result['link']
+    r = requests.get('http://127.0.0.1:9200/dmhy/torrent/_search?pretty=true&q=*:*&size=' + count)
+    process_result(r)
