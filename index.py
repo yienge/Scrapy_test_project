f0f47641228d2185dd140620c90f97af4471cdba
yienge <yienge@gmail.com>
2014-10-09 16:44:48 +0800

    fix

--- index.py
+++ index.py
@@ -6,92 +6,96 @@ import pprint
 # elasticsearch config example
 # https://gist.github.com/clintongormley/1088986
 
-payload = '七原罪'
 
-cmd = raw_input("cmd:")
+def main(cmd):
+    payload = '七原罪'
 
-if not cmd:
-    print 'plz enter cmd'
-    return
+    if not cmd:
+        print 'plz enter cmd, support commands are: analyze, setting, query, set_index'
+        return ''
 
-if cmd == 'analyze':
-    # test standard analyzer
-    r = requests.get('http://127.0.0.1:9200/_analyze?analyzer=standard', data=payload)
+    if cmd == 'analyze':
+        # test standard analyzer
+        r = requests.get('http://127.0.0.1:9200/_analyze?analyzer=standard', data=payload)
 
-    # test lowercase filter
-    # r = requests.get('http://127.0.0.1:9200/_analyze?tokenizer=keyword&filters=lowercase', data=payload)
+        # test lowercase filter
+        # r = requests.get('http://127.0.0.1:9200/_analyze?tokenizer=keyword&filters=lowercase', data=payload)
 
-    # test html_strip char filter
-    # r = requests.get('http://127.0.0.1:9200/_analyze?tokenizer=keyword&token_filters=lowercase&char_filters=html_strip', data=payload)
+        # test html_strip char filter
+        # r = requests.get('http://127.0.0.1:9200/_analyze?tokenizer=keyword&token_filters=lowercase&char_filters=html_strip', data=payload)
 
-    # test the tokenizer of the torrent index
-    # r = requests.get('http://127.0.0.1:9200/torrent/_analyze?text=this+is+a+test')
-elif cmd == 'setting':
-    # get index settings
-    r = requests.get('http://127.0.0.1:9200/torrent/_settings')
+        # test the tokenizer of the torrent index
+        # r = requests.get('http://127.0.0.1:9200/torrent/_analyze?text=this+is+a+test')
+    elif cmd == 'setting':
+        # get index settings
+        r = requests.get('http://127.0.0.1:9200/torrent/_settings')
 
-    # get all indices settings
-    # r = requests.get('http://127.0.0.1:9200/_all/_settings')
-elif cmd == 'query':
-    # request
-    r = requests.get('http://127.0.0.1:9200/torrent', data=payload)
-elif cmd == 'set_index':
-    # if you wanna change index analysis, you have to close, then change, and reopen index
-    settings_for_using_ngram = {
-        "mappings": {
-            "dmhy": {
-                "properties": {
-                    "url": {
-                        "type": "string",
-                        "analyzer": "url_ngram"
-                    },
-                    "link": {
-                        "type": "string"
-                    },
+        # get all indices settings
+        # r = requests.get('http://127.0.0.1:9200/_all/_settings')
+    elif cmd == 'query':
+        # request
+        r = requests.get('http://127.0.0.1:9200/torrent/_search', data=payload)
+    elif cmd == 'set_index':
+        # if you wanna change index analysis, you have to close, then change, and reopen index
+        settings_for_using_ngram = {
+            "mappings": {
+                "dmhy": {
+                    "properties": {
+                        "url": {
+                            "type": "string",
+                            "analyzer": "url_ngram"
+                        },
+                        "link": {
+                            "type": "string"
+                        },
+                    }
                 }
-            }
-        },
-        "settings": {
-            "analysis": {
-                "filter": {
-                    "url_ngram": {
-                        "side": "front",
-                        "min_gram": 1,
-                        "max_gram": 10,
-                        "type": "edgeNGram"
+            },
+            "settings": {
+                "analysis": {
+                    "filter": {
+                        "url_ngram": {
+                            "side": "front",
+                            "min_gram": 1,
+                            "max_gram": 10,
+                            "type": "edgeNGram"
+                        },
                     },
-                },
-                # "tokenizer": {
-                    # "custom_tokenizer": {
-                        # "type": "standard",
-                        # "max_token_length": 900,
-                        # "buffer_size": 512
-                    # }
-                # },
-                "analyzer": {
-                    "link_name": {
-                        "type": "custom",
-                        "tokenizer": "standard",
-                        "filter": [
-                            "standard",
-                            "lowercase",
-                            "url_ngram",
-                            "asciifolding"
-                        ]
-                    },
+                    # "tokenizer": {
+                        # "custom_tokenizer": {
+                            # "type": "standard",
+                            # "max_token_length": 900,
+                            # "buffer_size": 512
+                        # }
+                    # },
+                    "analyzer": {
+                        "link_name": {
+                            "type": "custom",
+                            "tokenizer": "standard",
+                            "filter": [
+                                "standard",
+                                "lowercase",
+                                "url_ngram",
+                                "asciifolding"
+                            ]
+                        },
+                    }
                 }
             }
         }
-    }
 
-    # close index
-    r = requests.get('http://127.0.0.1:9200/_all/_close')
+        # close index
+        r = requests.get('http://127.0.0.1:9200/_all/_close')
 
-    # change index analyzer
-    r = requests.put('http://127.0.0.1:9200/torrent/_settings', data=settings_for_using_ngram)
+        # change index analyzer
+        r = requests.put('http://127.0.0.1:9200/torrent/_settings', data=settings_for_using_ngram)
 
-    # open index
-    r = requests.get('http://127.0.0.1:9200/_all/_open')
+        # open index
+        r = requests.get('http://127.0.0.1:9200/_all/_open')
 
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(r.json())
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(r.json())
+
+
+cmd = raw_input("cmd:")
+main(cmd)
